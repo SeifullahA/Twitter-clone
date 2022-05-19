da3f77ef4576da21b51065abb3397c7958c9f5aa
Seifullah Abdullah <seifullahh@gmail.com>
2022-05-18 21:49:27 -0400

    final changes

--- posts/views.py
+++ posts/views.py
@@ -23,7 +23,7 @@ def index(request):
             return HttpResponseRedirect(form.errors.as_json())
 
     # Get all posts, limit = 20
-    posts = Post.objects.all()[:20]
+    posts = Post.objects.all().order_by('-created_at')[:20]
 
     # Show
     return render(request, 'posts.html',
